9700be7ffaa52727fc7f059af07f2c679fab8e77
ChaoLi7 <58619068+ChaoLi7@users.noreply.github.com>
2024-05-30 09:59:31 +0800

    Update Corr.R

--- Visualization/corr/Corr.R
+++ Visualization/corr/Corr.R
@@ -1,1 +1,19 @@
-
+#expression correlation matrix
+			 
+  setwd("/bin/expression")
+  test <- read.table(file='expression.tsv',header = T)
+  expression_corr <- cor(test[,2:27], method = 'spearman')  
+  
+  library(pheatmap)
+  library(RColorBrewer)
+  b<-as.matrix(expression_corr)
+  pheatmap(b,
+           cluster_rows = T,
+           cluster_cols = T,
+           border_color="NA",
+           show_rownames = TRUE, 
+           color = colorRampPalette(c("#FFFFC8","#F5A100","#7D0025"))(50),
+           main = 'Heatmap')
+  #boarder
+  library(corrplot)
+  corrplot(corr = b)
